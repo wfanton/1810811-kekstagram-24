028ac1f682094fe3d9515a37422625ed0758bb6d
Tommy <wfanton@mail.ru>
2021-10-13 21:17:44 +0300

    add two functions3

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function getRandomNumber(min, max) {
-  const random = min + Math.random() * (max - min);
-  return Math.round(random);
+  const random = min + Math.random() * (max + 1 - min);
+  return Math.floor(random);
 }
 
 getRandomNumber(3, 15);
 
 
 function checkCommentLength (string, lenght) {
-  if (string.length <= lenght) {
+  if (string.lenght <= lenght) {
     return true;
   }
 }
